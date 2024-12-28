b2a1f9549eec03b470361e12835a897133aecb29
nrcool <nomanalam7214@gmail.com>
2024-12-28 22:50:46 +0100

    add media query classes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,11 +3,16 @@ import { Navigation } from './Navigation';
 
 export function Header() {
   return (
-    <header className="bg-dark border border-white-700 mx-2 sm:m-3">
-      <div className="max-w-6xl mx-auto px-3 sm:px-6 py-3 sm:py-4 flex justify-between items-center">
-        <Logo />
-        <Navigation />
-      </div>
-    </header>
+   
+    <nav className="border-b border-gray-800 bg-[#161b22]/80 backdrop-blur-sm sticky top-0 z-50">
+            <div className="container mx-auto px-4">
+              <div className="flex justify-between h-16 items-center">
+              <Logo />
+        <Navigation /> 
+               
+              </div>
+            </div>
+          </nav> 
   );
 }
+
